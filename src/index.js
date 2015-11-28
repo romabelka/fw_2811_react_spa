8443e70ad44c43a9b0200de7a821586b661a4172
Sergey Sokurenko <s.sokurenko@gmail.com>
2015-11-28 11:33:59 +0100

    Moved App component to a separate file.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import Article from './components/Article'
-
-const article = {
-  title: 'Some title',
-  text: 'Article body text'
-}
-
-class App extends Component {
-  render(){
-    return(
-      <div>
-        <h1>React App</h1>
-        <Article article={article}/>
-      </div>
-    )
-  }
-}
+import App from './components/App'
 
 ReactDOM.render(<App/>, document.getElementById("container"));
